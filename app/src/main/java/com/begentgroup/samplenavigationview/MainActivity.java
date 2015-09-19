650d5f0b23f5b9fd96be4c2bea5f9bc639fa9b59
dongja94 <dongja94@gmail.com>
2015-09-19 22:22:19 +0900

    Initial commit

---
+++ app/src/main/java/com/begentgroup/samplenavigationview/MainActivity.java
@@ -0,0 +1,65 @@
+package com.begentgroup.samplenavigationview;
+
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements
+        NavigationView.OnNavigationItemSelectedListener {
+
+    DrawerLayout drawerView;
+    NavigationView menuView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        drawerView = (DrawerLayout)findViewById(R.id.drawer);
+        menuView = (NavigationView)findViewById(R.id.navigationView);
+        menuView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem menuItem) {
+        switch(menuItem.getItemId()) {
+            case R.id.navi_menu1 :
+            case R.id.navi_menu2 :
+                Toast.makeText(this, "Navi Menu Select", Toast.LENGTH_SHORT).show();
+                menuItem.setChecked(true);
+                break;
+            case R.id.navi_sub_menu1 :
+            case R.id.navi_sub_menu2 :
+                Toast.makeText(this, "Navi Sub Menu Select", Toast.LENGTH_SHORT).show();
+                break;
+
+        }
+        drawerView.closeDrawers();
+        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
